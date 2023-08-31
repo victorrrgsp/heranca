095300b058c9930c4519699b1057fc897aea6a52
Victorgsp <victorgabrielpimenta52@gmail.com>
2023-08-31 14:31:16 -0300

    Polimofismo

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -8,17 +8,14 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Account acc1 = new Account(1001, "Alex", 1000.0);
-		acc1.withdraw(200.0);
+		//polimofismo
+		Account acc1 = new Account(1002, "Maria", 1000.0);
+		Account acc2 = new SavingsAccount(1003, "Alex", 1000.00, 0.01);
+
+		acc1.withdraw(50.0);
+		acc2.withdraw(50.0);
+
 		System.out.println(acc1.getBalance());
-
-		//prevalece o que está na sub classe SavingsAccount sem a taxa no valor de 5 
-		Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
-		acc2.withdraw(200.0);
 		System.out.println(acc2.getBalance());
-
-		Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
-		acc3.withdraw(200.0);
-		System.out.println(acc3.getBalance());
 	}
 }
